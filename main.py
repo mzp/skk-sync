c7af00e87a05c10167015ad7d05ec9729ca36f2a
mzp <mzpppp@gmail.com>
2009-03-25 14:46:54 +0900

    implement word add/index

--- main.py
+++ main.py
@@ -4,6 +4,22 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.api import users
 from google.appengine.ext.webapp.util import login_required
+from django import newforms
+from django.newforms import models
+from google.appengine.ext import db
+from google.appengine.ext.db import djangoforms
+
+class Word(db.Model):
+  user = db.UserProperty()
+  word = db.StringProperty(verbose_name="単語",required=True)
+  yomi = db.StringProperty(verbose_name="読み",required=True)
+  date = db.DateTimeProperty(auto_now_add=True)
+  annotation = db.StringProperty(verbose_name="注釈",required=False)
+
+class WordForm(djangoforms.ModelForm):
+  class Meta:
+    model = Word
+    exclude = ['user']
 
 class BaseHandler(webapp.RequestHandler):
   def render(self,name,context={}):
@@ -25,11 +41,46 @@ class HomeHandler(BaseHandler):
   @login_required
   def get(self):
     user = users.get_current_user()
-    self.render('home',{'nickname': user.nickname()})
+    words = db.GqlQuery("SELECT * FROM Word WHERE user = :1 ORDER BY yomi ASC",
+                        user)
+    count = words.count()
+    last_mod = db.GqlQuery("SELECT * FROM Word WHERE user = :1 ORDER BY date DESC LIMIT 1",
+                           user).get().date
+    self.render('home',{
+        'nickname': user.nickname(),
+        'words': words,
+        'words_count':count,
+        'last_mod':last_mod
+        })
+
+class DictHandler(BaseHandler):
+  def post(self):
+    user = users.get_current_user()
+    if not user:
+      self.redirect(users.create_login_url('/'))
+    form = WordForm(self.request)
+    if form.is_valid():
+      model = form.save(commit=False)
+      model.user = user
+      model.put()
+      self.redirect('/home')
+    else:
+      self.render('dict_add',{
+          'form': form
+          })
+
+  @login_required
+  def get(self):
+    form = WordForm()
+    self.render('dict_add',{
+        'form': form
+        })
+
 
 def main():
   application = webapp.WSGIApplication([('/', MainHandler),
-                                        ('/home',HomeHandler)
+                                        ('/home',HomeHandler),
+                                        ('/dict/add',DictHandler)
                                         ],
                                        debug=True)
   wsgiref.handlers.CGIHandler().run(application)
